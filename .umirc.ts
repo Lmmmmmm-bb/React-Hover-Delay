e6efe25a6463239489700c6fdfffb43c3151302a
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-07-09 20:47:59 +0800

    fix: docs deploy base url

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/React-Hover-Delay',
+  publicPath: '/React-Hover-Delay/',
   title: 'react-hover-delay',
   favicon: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
